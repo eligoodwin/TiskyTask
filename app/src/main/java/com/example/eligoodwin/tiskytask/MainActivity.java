5be8821a4b8bf271af52d3a6b879e10af1ee22d5
eligoodwin <goodwiel@oregonstate.edu>
2017-12-13 12:28:24 -0800

    it works

--- app/src/main/java/com/example/eligoodwin/tiskytask/MainActivity.java
+++ app/src/main/java/com/example/eligoodwin/tiskytask/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.eligoodwin.tiskytask;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Handler pauseSplash = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pauseSplash.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent makeTasks = new Intent(MainActivity.this, CreateTodoList.class);
+                startActivity(makeTasks);
+            }
+        }, 2000);
 
     }
 }
